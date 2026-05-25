39a0bf345877b475d3b490894e4f455a6acdb61a
enigmatic-mind <enigmatic-mind@users.noreply.github.com>
2026-05-25 16:45:00 -0700

    L6 code breaker

---
+++ L6 - CodeBreaker/CodeBreaker/CodeBreaker.swift
@@ -0,0 +1,114 @@
+//
+//  CodeBreaker.swift
+//  CodeBreaker
+//
+//  Created by Francisco on 5/20/26.
+//
+
+import SwiftUI
+
+typealias Peg = Color
+
+struct CodeBreaker {
+    var masterCode: Code = Code(kind: .master)
+    var guess: Code = Code(kind: .guess)
+    var attempts: [Code] = []
+    let pegChoices: [Peg]
+    
+    init(pegChoices: [Peg] = [.red, .green, .blue, .yellow]) {
+        self.pegChoices = pegChoices
+        masterCode.randomize(from: pegChoices)
+    }
+    
+    mutating func attemptGuess() {
+        var attempt = guess
+        attempt.kind = .attempt(guess.match(against: masterCode))
+        attempts.append(attempt)
+    }
+    
+    mutating func changeGuessPeg(at index: Int) {
+        let existingPeg = guess.pegs[index]
+        if let indexOfExistingPegInPegChoices = pegChoices.firstIndex(of: existingPeg) {
+            let newPeg = pegChoices[(indexOfExistingPegInPegChoices + 1) % pegChoices.count]
+            guess.pegs[index] = newPeg
+        } else {
+            guess.pegs[index] = pegChoices.first ?? Code.missingPeg
+        }
+    }
+}
+
+struct Code {
+    var kind: Kind
+    var pegs: [Peg] = Array(repeating: Code.missingPeg, count: 4)
+    
+    static let missingPeg: Peg = .clear
+    
+    enum Kind: Equatable {
+        case master
+        case guess
+        case attempt([Match])
+        case unknown
+    }
+    
+    mutating func randomize(from pegChoices: [Peg]) {
+        for index in pegChoices.indices {
+            pegs[index] = pegChoices.randomElement() ?? Code.missingPeg
+        }
+    }
+    
+    var matches: [Match]? {
+        switch kind {
+        case .attempt(let matches): return matches
+        default: return nil
+        }
+    }
+    
+    // TODO: Review
+    //    func match(against otherCode: Code) -> [Match] {
+    //        var results: [Match] = Array(repeating: .noMatch, count: pegs.count)
+    //        var pegsToMatch = otherCode.pegs
+    //        for index in pegs.indices.reversed() {
+    //            if pegsToMatch.count > index, pegsToMatch[index] == pegs[index] {
+    //                results[index] = .exact
+    //                pegsToMatch.remove(at: index)
+    //            }
+    //        }
+    //
+    //        for index in pegs.indices {
+    //            if results[index] != .exact {
+    //                if let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
+    //                    results[index] = .inexact
+    //                    pegsToMatch.remove(at: matchIndex)
+    //                }
+    //            }
+    //        }
+    //        return results
+    //    }
+    
+    // TODO: understand
+    func match(against otherCode: Code) -> [Match] {
+        var pegsToMatch = otherCode.pegs
+        
+        let backwardExactMatches = pegs.indices.reversed().map { index in
+            if pegsToMatch.count > index, pegsToMatch[index] == pegs[index] {
+                pegsToMatch.remove(at: index)
+                return Match.exact
+            } else {
+                return .noMatch
+            }
+        }
+        
+        let exactMatches = Array(backwardExactMatches.reversed())
+        return pegs.indices.map { index in
+            if exactMatches[index] != .exact, let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
+                pegsToMatch.remove(at: matchIndex)
+                return .inexact
+            } else {
+                return exactMatches[index]
+            }
+        }
+        
+    }
+}
+
+
---
+++ L6 - CodeBreaker/CodeBreaker/CodeBreakerView.swift
@@ -0,0 +1,78 @@
+//
+//  CodeBreakerView.swift
+//  CodeBreaker
+//
+//  Created by Francisco on 5/4/26.
+//
+
+import SwiftUI
+
+struct CodeBreakerView: View {
+    @State var game = CodeBreaker(pegChoices: [.brown, .yellow, .orange, .black])
+    
+    var body: some View {
+        VStack {
+            view(for: game.masterCode)
+            ScrollView {
+                view(for: game.guess)
+                ForEach(game.attempts.indices.reversed(), id: \.self) { index in
+                    view(for: game.attempts[index])
+                }
+            }
+            
+        }
+        .padding()
+    }
+    
+    var guessButton: some View {
+        Button {
+            withAnimation {
+                game.attemptGuess()
+            }
+        } label: {
+            Text("Guess")
+        }
+        .font(.system(size: 80))
+        .minimumScaleFactor(0.1)
+    }
+    
+    func view(for code: Code) -> some View {
+        HStack {
+            ForEach(code.pegs.indices, id: \.self) { index in
+                RoundedRectangle(cornerRadius: 10)
+                    .overlay {
+                        if code.pegs[index] == Code.missingPeg {
+                            RoundedRectangle(cornerRadius: 10)
+                                .strokeBorder(Color.gray)
+                        }
+                    }
+                    .contentShape(Rectangle())
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundStyle(code.pegs[index])
+                    .onTapGesture {
+                        if code.kind == .guess {
+                            game.changeGuessPeg(at: index)
+                        }
+                    }
+            }
+            
+            Rectangle()
+                .foregroundStyle(Color.clear)
+                .aspectRatio(1, contentMode: .fit)
+                .overlay {
+                    if let matches = code.matches {
+                        MatchMarkers(matches: matches)
+                    } else {
+                        if code.kind == .guess {
+                            guessButton
+                        }
+                    }
+                }
+        }
+    }
+}
+
+
+#Preview {
+    CodeBreakerView()
+}
